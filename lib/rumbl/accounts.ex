9dba5804042f0b018b4b5671355418d434d1923b
Tad Preston <tadpreston@gmail.com>
2019-07-05 12:31:59 -0500

    Added ability to create a user

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,25 +3,32 @@ defmodule Rumbl.Accounts do
 @moduledoc """
 The Accounts context.
 """
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    list_users()
-    |> Enum.find(fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    list_users()
-    |> Enum.find(fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users() do
+    Repo.all(User)
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
 end
