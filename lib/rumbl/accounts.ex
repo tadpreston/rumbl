debe6c3e683a13a37152ace39b0e1a1609e6c698
Tad Preston <tadpreston@gmail.com>
2019-07-07 07:01:10 -0500

    Added rough authentication"

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -5,6 +5,9 @@ The Accounts context.
 """
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
+  alias Rumbl.Accounts.Credential
+
+  import Ecto.Query
 
   def get_user(id) do
     Repo.get(User, id)
@@ -30,5 +33,61 @@ The Accounts context.
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Argon2.check_pass(user, given_pass) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Argon2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
+
+  def list_credentials do
+    Repo.all(Credential)
+  end
+
+  def get_credential!(id), do: Repo.get!(Credential, id)
+
+  def create_credential(attrs \\ %{}) do
+    %Credential{}
+    |> Credential.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_credential(%Credential{} = credential, attrs) do
+    credential
+    |> Credential.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_credential(%Credential{} = credential) do
+    Repo.delete(credential)
+  end
+
+  def change_credential(%Credential{} = credential) do
+    Credential.changeset(credential, %{})
   end
 end
